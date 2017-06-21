549c1f2091728e45699653e0123ce98083645023
Glissando <justinjoelward@gmail.com>
2017-06-21 06:40:25 -0500

    dss

--- js/Lemonade.js
+++ js/Lemonade.js
@@ -59,6 +59,7 @@ BasicApp.Lemonade.prototype = {
 
 		for(var i=0;i*this.cellSize < this.game.width;i++){
 			var pos = this.cellSize * i;
+			graphics.beginFill(0xffffff);
 			graphics.moveTo(pos, 0);
 			//graphics.lineTo(pos, this.game.height);
 			//var line = new Phaser.Line(pos, 0, pos, this.game.height);
@@ -69,6 +70,7 @@ BasicApp.Lemonade.prototype = {
 
 		for(var i=0;i*this.cellSize < this.game.height;i++){
 			var pos = this.cellSize * i;
+			graphics.beginFill(0xffffff);
 			graphics.moveTo(0, pos);
 			//graphics.lineTo(this.game.width, pos);
 			//var line = new Phaser.Line(0, pos, this.game.width, pos);
