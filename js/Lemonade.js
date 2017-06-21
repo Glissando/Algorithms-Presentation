ae2cbf37781e6b5a1c408ce3ee9f436cdee0468a
Glissando <justinjoelward@gmail.com>
2017-06-21 07:17:20 -0500

    easda

--- js/Lemonade.js
+++ js/Lemonade.js
@@ -190,14 +190,23 @@ BasicApp.Lemonade.prototype = {
 		this.esc.onDown.add(this.menu, this);
 	},
 
+	distance: function (x1, y1, x2, y2) {
+
+        var dx = x1 - x2;
+        var dy = y1 - y2;
+
+        return Math.sqrt(dx * dx + dy * dy);
+
+    },
+
 	leftClick: function(){
 		var x = this.input.activePointer.x;
 		var y = this.input.activePointer.y;
 
 		var latticePoint = this.latticePosition(x,y);
 
-		console.log(Phaser.point.distance(latticePoint, new Phaser.Point(x/this.cellSize,y/this.cellSize)));
-		if(Phaser.Point.distance(latticePoint, new Phaser.Point(x/this.cellSize,y/this.cellSize)) < this.latticeRange){
+		console.log(Phaser.point.distance(latticePoint.x, latticePoint.y, x/this.cellSize,y/this.cellSize));
+		if(this.distance(latticePoint.x, latticePoint.y, x/this.cellSize, y/this.cellSize) < this.latticeRange){
 			if(this.siteMode){
 
 				if(!this.removeSite(latticePoint)){
